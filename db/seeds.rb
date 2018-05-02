5ca389f6c76fa5ac5ca5603e1c1027f5d965fc19
ipoca85 <hong.chen@ipoca.jp>
2018-05-02 16:21:38 +0900

    cart bug fix.

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,8 @@ Product.create!(title: 'Rails, Angular, Postgres, and Bootstrap',
       environment.
       </p>},
   image_url: 'dcbang.jpg',
-  price: 45.00)
+  price: 45.00
+  product_id: 1)
 # . . .
 Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
   description:
@@ -35,7 +36,8 @@ Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
       out which cross-platform solution makes the most sense for your needs.
       </p>},
   image_url: '7apps.jpg',
-  price: 26.00)
+  price: 26.00
+  product_id: 2)
 # . . .
 
 Product.create!(title: 'Ruby Performance Optimization',
@@ -52,4 +54,5 @@ Product.create!(title: 'Ruby Performance Optimization',
       will run orders of magnitude faster.
       </p>},
   image_url: 'adrpo.jpg',
-  price: 46.00)
+  price: 46.00
+  product_id: 3)
